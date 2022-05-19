250cfb213a7737ddc32c23f33683671c5a07507f
Deven Rathod <devenrathod2001@gmail.com>
2022-05-20 00:02:03 +0530

    Added Live API

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,59 +1,68 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { Box , Center, Text} from "@chakra-ui/react"
-import logo from '../public/logo.svg'
-import axios from 'axios'
-import { useState } from 'react'
-
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Box, Center, Text } from "@chakra-ui/react";
+import logo from "../public/logo.svg";
+import axios from "axios";
+import { useState } from "react";
+import CountUp from "react-countup";
+import { AiFillStar, AiFillGithub } from "react-icons/ai";
 
 
 export default function Home() {
   const [starts, setStarts] = useState(null);
 
-  
-  axios.get('https://api.github.com/repos/appwrite/appwrite').then(response => {
-  console.log(response);
-  console.log(response.data.stargazers_count)
-  setStarts(response.data.stargazers_count)
-});
+  axios
+    .get("https://api.github.com/repos/appwrite/appwrite")
+    .then((response) => {
+      console.log(response);
+      console.log(response.data.stargazers_count);
+      setStarts(response.data.stargazers_count);
+    });
 
   return (
-    <Box 
-      bg="#f02e65"
-      height="100vh"
-      width="100vw"
-    >
+    <Box bg="#f02e65" height="100vh" width="100vw">
       <Center>
-        <Image 
-        src={logo}
-        alt="logo"
-        width="600px"
-        ></Image>
+        <Image src={logo} alt="logo" width="600px"></Image>
       </Center>
       <Box>
-        <Center>
-          <Text
-            fontSize="6rem"
-            fontWeight="bold"
-            color="#fff"
-            textAlign="center"
+        <Box>
+          <CountUp
+            start={0}
+            end={starts}
+            duration={2.75}
+            separator=" "
+            decimals={0}
+            decimal=","
           >
-            {starts}
-          </Text>
-        </Center>
-        <Center>
+            {({ countUpRef, start }) => (
+              <div>
+                <Center>
+                  <Text
+                    ref={countUpRef}
+                    fontSize="4rem"
+                    fontWeight="bold"
+                    color="#fff"
+                    textAlign="center"
+                  ><AiFillStar></AiFillStar></Text>
+                </Center>
+              </div>
+            )}
+          </CountUp>
+        </Box>
+      </Box>
+      <Box>
+      <Center>
         <Text
-            fontSize="6rem"
+            fontSize="4rem"
             fontWeight="bold"
             color="#fff"
             textAlign="center"
           >
-            Github Stars
+        Github Star-gazers
           </Text>
         </Center>
-        
       </Box>
     </Box>
-  )
+  );
 }
